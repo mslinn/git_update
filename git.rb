800b4d4f680b664c3a80d29346756ac1e53c8b83
Mike Slinn <mslinn@mslinn.com>
2023-05-06 23:32:04 -0400

    -

---
+++ git.rb
@@ -0,0 +1,36 @@
+require 'rugged'
+
+def fetch(repo)
+  remote = repo.remotes['origin']
+  remote.connect(:fetch) do |r|
+    r.download
+    r.update_tips!
+  end
+end
+
+def merge(repo)
+  merge_index = repo.merge_commits(
+    Rugged::Branches.lookup(repo, 'master').tip,
+    Rugged::Branches.lookup(repo, 'origin/master').tip
+  )
+
+  raise 'Conflict detected!' if merge_index.conflicts?
+
+  merge_commit = Rugged::Commit.create(repo, {
+    parents:    [
+                  Rugged::Branches.lookup(repo, 'master').tip,
+                  Rugged::Branches.lookup(repo, 'origin/master').tip
+                ],
+    tree:       merge_index.write_tree(repo),
+    message:    'Merged `origin/master` into `master`',
+    author:     { name: "User", email: "example@test.com" },
+    committer:  { name: "User", email: "example@test.com" },
+    update_ref: 'master'
+  })
+end
+
+def pull
+  repo = Rugged::Repository.new dir_name
+  fetch repo
+  merge repo
+end
--- update.rb
+++ update.rb
@@ -1,53 +1,29 @@
-require 'rugged'
 require_relative 'util'
 
 @threads = []
 
 def process_dir(dir_name)
-  return if File.exist? "#{dir_name}/.ignore"
+  if File.exist? "#{dir_name}/.ignore"
+    puts "Ignoring #{dir_name}"
+    return
+  end
 
   if Dir.exist? "#{dir_name}/.git"
     @threads << Thread.new { update dir_name }
     return
   end
-  Dir['dir_name/**'].each do |entry|
-    process_dir entry if File.directory? entry
+
+  puts "Processing #{dir_name}"
+  Dir["#{dir_name}/*"].each do |entry|
+    process_dir(entry) if File.directory? entry
   end
-end
-
-def fetch(repo)
-  remote = repo.remotes['origin']
-  remote.connect(:fetch) do |r|
-    r.download
-    r.update_tips!
-  end
-end
-
-def merge(repo)
-  merge_index = repo.merge_commits(
-    Rugged::Branches.lookup(repo, 'master').tip,
-    Rugged::Branches.lookup(repo, 'origin/master').tip
-  )
-
-  raise 'Conflict detected!' if merge_index.conflicts?
-
-  merge_commit = Rugged::Commit.create(repo, {
-    parents:    [
-                  Rugged::Branches.lookup(repo, 'master').tip,
-                  Rugged::Branches.lookup(repo, 'origin/master').tip
-                ],
-    tree:       merge_index.write_tree(repo),
-    message:    'Merged `origin/master` into `master`',
-    author:     { name: "User", email: "example@test.com" },
-    committer:  { name: "User", email: "example@test.com" },
-    update_ref: 'master'
-  })
 end
 
 def update(dir_name)
-  repo = Rugged::Repository.new dir_name
-  fetch repo
-  merge repo
+  Dir.chdir(dir_name) do
+    Updating "git pull #{dir_name}"
+    `git pull`
+  end
 end
 
 abort "Error: no directory specified" if ARGV.empty?
