0ee62c3632a64d7dfc64b01330085831c2562f71
Mike Slinn <mslinn@mslinn.com>
2023-05-09 12:46:04 -0400

    -

--- spec/pull_spec.rb
+++ spec/pull_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe('Pull') do
   it 'is created properly' do
     repo = Rugged::Repository.new base_fq
     remote = repo.remotes['origin']
+    credentials = ssh_credentials repo
     success = remote.check_connection(:fetch, credentials: credentials)
     expect(success).to be true
+  rescue StandardError => e
+    flunk e.message
   end
 end
