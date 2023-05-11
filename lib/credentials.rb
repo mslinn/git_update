bba88041e60b7d0c37aa0e073b17ec78773c7f92
Mike Slinn <mslinn@mslinn.com>
2023-05-11 18:30:25 -0400

    -

--- lib/credentials.rb
+++ lib/credentials.rb
@@ -1,48 +1,58 @@
 require 'rainbow/refinement'
 require 'rugged'
 
-using Rainbow
+class GitUpdate
+  using Rainbow
 
-# Allowable credential types:
-#   :plaintext       - A vanilla user/password request
-#   :ssh_custom      - An SSH key-based authentication request with a custom signature
-#   :default         - An NTLM/Negotiate-based authentication request.
-#   :ssh_interactive - An SSH interactive authentication request
-#   :ssh_memory      - Credentials read from memory. Only available for libssh2+OpenSSL.
-#                      An SSH key-based authentication request. Allows credentials to be read
-#                      from memory instead of files. Note that because of differences in crypto
-#                      backend support, it might not be functional.
-#   :username        - Username-only authentication request. Used as a pre-authentication step if
-#                      the underlying transport (eg. SSH, with no username in its URL) does not
-#                      know which username to use.
-# See https://github.com/libgit2/libgit2/blob/v1.6.4/include/git2/credential.h#L27-L79
+  # Allowable credential types for libgit2:
+  #   :plaintext       - A vanilla user/password request
+  #   :ssh_key         - An SSH key-based authentication request
+  #   :ssh_custom      - An SSH key-based authentication request with a custom signature
+  #   :default         - An NTLM/Negotiate-based authentication request.
+  #   :ssh_interactive - An SSH interactive authentication request
+  #   :ssh_memory      - Credentials read from memory. Only available for libssh2+OpenSSL.
+  #                      An SSH key-based authentication request. Allows credentials to be read
+  #                      from memory instead of files. Note that because of differences in crypto
+  #                      backend support, it might not be functional.
+  #   :username        - Username-only authentication request. Used as a pre-authentication step if
+  #                      the underlying transport (eg. SSH, with no username in its URL) does not
+  #                      know which username to use.
+  # See https://github.com/libgit2/libgit2/blob/v1.6.4/include/git2/credential.h#L27-L79
+  #
+  # However, it looks like rugged only uses:
+  #   :plaintext       - A vanilla user/password request
+  #   :ssh_key         - An SSH key-based authentication request
+  #   :default         - An NTLM/Negotiate-based authentication request.
+  # See https://github.com/libgit2/rugged/blob/v1.6.3/lib/rugged/credentials.rb
+  # and https://github.com/libgit2/rugged/blob/v1.6.3/ext/rugged/rugged_cred.c#L127-L130
 
-@select_credentials = proc do |url, username, allowable_credential_types|
-  # allowable_credential_types is an array of symbols that often contains :ssh_key or :username.
-  puts "  Computing credentials for #{username} at #{url}; allowable credential types are: #{allowable_credential_types}".blue.bright
-  if url.start_with?('https') # && allowable_credential_types.include?(:plaintext)
-    # The values of username and password are ignored for public repos on GitHub, GitLab, Bitbucket, etc.
-    # However, the parameters must be provided even though the values are ignored
-    # See https://learn.microsoft.com/en-us/azure/devops/repos/git/set-up-credential-managers
-    Rugged::Credentials::UserPassword.new(username: '', password: '')
-  elsif url.start_with?('git@', 'ssh://') && allowable_credential_types.include?(:ssh_key)
-    begin
-      Rugged::Credentials::SshKey.new(
-        username:   'git',
-        passphrase: nil,
-        publickey:  File.expand_path('~/.ssh/id_rsa.pub'), # TODO: figure out which cert to use
-        privatekey: File.expand_path('~/.ssh/id_rsa')
-      )
-    rescue StandardError => e
-      puts "  SshKey credentials failed, trying ssh agent: #{e.full_message}".blue.bright
+  @select_credentials = proc do |url, username, allowable_credential_types|
+    # allowable_credential_types is an array of symbols that often contains :ssh_key or :username.
+    puts "  Computing credentials for #{username} at #{url}; allowable credential types are: #{allowable_credential_types}".blue.bright
+    if url.start_with?('https') # && allowable_credential_types.include?(:plaintext)
+      # The values of username and password are ignored for public repos on GitHub, GitLab, Bitbucket, etc.
+      # However, the parameters must be provided even though the values are ignored
+      # See https://learn.microsoft.com/en-us/azure/devops/repos/git/set-up-credential-managers
+      Rugged::Credentials::UserPassword.new(username: '', password: '')
+    elsif url.start_with?('git@', 'ssh://') && allowable_credential_types.include?(:ssh_key)
       begin
-        Rugged::Credentials::SshKeyFromAgent.new(username: 'git')
+        Rugged::Credentials::SshKey.new(
+          username:   'git',
+          passphrase: nil,
+          publickey:  File.expand_path('~/.ssh/id_rsa.pub'), # TODO: figure out which cert to use
+          privatekey: File.expand_path('~/.ssh/id_rsa')
+        )
       rescue StandardError => e
-        puts "Ssh agent failed: #{e.full_message}".red
-        raise e
+        puts "  SshKey credentials failed, trying ssh agent: #{e.full_message}".blue.bright
+        begin
+          Rugged::Credentials::SshKeyFromAgent.new(username: 'git')
+        rescue StandardError => e
+          puts "Ssh agent failed: #{e.full_message}".red
+          raise e
+        end
       end
+    else
+      puts "  Error: Unknown credential types: #{allowable_credential_types}".red
     end
-  else
-    puts "  Error: Unknown credential types: #{allowable_credential_types}".red
   end
 end
--- lib/pull.rb
+++ lib/pull.rb
@@ -2,53 +2,60 @@ require 'rainbow/refinement'
 require 'rugged'
 require_relative 'credentials'
 
-using Rainbow
+class GitUpdate
+  using Rainbow
 
-abort "Error: Rugged was not built with ssh support. Please see https://www.mslinn.com/git/4400-rugged.html".red \
-  unless Rugged.features.include? :ssh
+  abort "Error: Rugged was not built with ssh support. Please see https://www.mslinn.com/git/4400-rugged.html".red \
+    unless Rugged.features.include? :ssh
 
-# Just updates the default branch
-def pull(repo, remote_name = 'origin')
-  remote = repo.remotes[remote_name]
-  default_branch = repo.head.name.split('/')[-1]
-  refspec_str = "refs/remotes/#{remote_name}/#{default_branch}"
-  begin
-    # remote.check_connection(:fetch, credentials: @select_credentials)
-    remote.fetch(refspec_str, credentials: @select_credentials)
-  rescue Rugged::NetworkError => e
-    puts "  Error: #{e.full_message}".red
+  # Just updates the default branch
+  def pull(repo, remote_name = 'origin') # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    remote = repo.remotes[remote_name]
+    puts "  remote.url=#{remote.url}".yellow
+    default_branch = repo.head.name.split('/')[-1]
+    refspec_str = "refs/remotes/#{remote_name}/#{default_branch}"
+    begin
+      success = remote.check_connection(:fetch, credentials: @select_credentials)
+      unless success
+        puts "  Error: remote.check_connection failed".red
+        return
+      end
+      remote.fetch(refspec_str, credentials: @select_credentials)
+    rescue Rugged::NetworkError => e
+      puts "  Error: #{e.full_message}".red
+    end
+    abort "Error: repo.ref(#{refspec_str}) for #{remote} is nil".red if repo.ref(refspec_str).nil?
+    remote_master_id = repo.ref(refspec_str).target
+    merge_result, = repo.merge_analysis(remote_master_id)
+
+    case merge_result
+    when :up_to_date
+      # Nothing needs to be done
+      puts "  Repo at '#{repo.workdir}' was already up to date.".blue.bright
+
+    when :fastforward
+      repo.checkout_tree(repo.get(remote_master_id))
+      master_ref = repo.lookup_reference('refs/heads/master')
+      master_ref.set_target(remote_master_id)
+      repo.head.set_target(remote_master_id)
+
+    when :normal
+      repo.merge(remote_master_id)
+      raise "Problem: merging updates for #{repo.name} encountered conflicts".red if repo.index.conflicts?
+
+      user = repo.default_signature
+      tree = repo.index.write_tree
+      repo.create_commit 'HEAD', user, user, 'Merge', tree, [repo.head.target, remote_master_id]
+      repo.state_cleanup
+    else
+      raise AssertionError 'Unknown merge analysis result'.red
+    end
   end
-  abort "Error: repo.ref(#{refspec_str}) for #{remote} is nil".red if repo.ref(refspec_str).nil?
-  remote_master_id = repo.ref(refspec_str).target
-  merge_result, = repo.merge_analysis(remote_master_id)
 
-  case merge_result
-  when :up_to_date
-    # Nothing needs to be done
-    puts "  Repo at '#{repo.workdir}' was already up to date.".blue.bright
-
-  when :fastforward
-    repo.checkout_tree(repo.get(remote_master_id))
-    master_ref = repo.lookup_reference('refs/heads/master')
-    master_ref.set_target(remote_master_id)
-    repo.head.set_target(remote_master_id)
-
-  when :normal
-    repo.merge(remote_master_id)
-    raise "Problem: merging updates for #{repo.name} encountered conflicts".red if repo.index.conflicts?
-
-    user = repo.default_signature
-    tree = repo.index.write_tree
-    repo.create_commit 'HEAD', user, user, 'Merge', tree, [repo.head.target, remote_master_id]
-    repo.state_cleanup
-  else
-    raise AssertionError 'Unknown merge analysis result'.red
+  def update_via_rugged(dir_name)
+    repo = Rugged::Repository.new dir_name
+    pull repo
+  rescue StandardError => e
+    puts "Ignoring #{dir_name} due to error: .#{e.full_message}".red
   end
-end
-
-def update_via_rugged(dir_name)
-  repo = Rugged::Repository.new dir_name
-  pull repo
-rescue StandardError => e
-  puts "Ignoring #{dir_name} due to error: .#{e.full_message}".red
 end
